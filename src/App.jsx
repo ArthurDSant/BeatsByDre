561a15fc47813637a75ae20ef53414034675710b
ArthurSantDev <devarthursant@gmail.com>
2024-06-30 16:56:40 -0300

    finaly

--- src/App.jsx
+++ src/App.jsx
@@ -110,6 +110,7 @@ const App = () => {
 
     sr.reveal(`.home__header, .section__title`, { delay: 600 });
     sr.reveal(`.home__footer`, { delay: 700 });
+    sr.reveal(`.home__img`, {origin: 'top' });
 
     sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`, { origin: 'top', interval: 100 });
     sr.reveal(`.specs__data, .discount__animate`, { origin: 'left', interval: 100 });
